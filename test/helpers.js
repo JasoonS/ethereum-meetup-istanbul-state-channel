fa9cf57781b64c96f4fa7019ed1f7aab936004e2
Jason Smythe <jason@smythemail.za.net>
2018-10-24 17:30:30 +0300

    Fix tests - sync up

--- test/helpers.js
+++ test/helpers.js
@@ -4,4 +4,13 @@ const should = require('chai')
     .use(require('chai-bignumber')(BigNumber))
     .should()
 const EVMThrow = 'invalid opcode'
-module.exports = { should, EVMThrow }
+const assertRevert = async promise => {
+  try {
+    await promise
+    assert.fail('Expected revert not received')
+  } catch (error) {
+    const revertOrAssertFound = error.message.search('revert') >= 0 || error.message.search('assert') >= 0
+    assert(revertOrAssertFound, `Expected "revert", got ${error} instead`)
+  }
+}
+module.exports = { should, EVMThrow, assertRevert }
--- test/test.js
+++ test/test.js
@@ -1,49 +1,49 @@
-const timeManager = require('openzeppelin-solidity/test/helpers/increaseTime');
-const currentTime = require('openzeppelin-solidity/test/helpers/latestTime');
-// const {should} = require("./helpers");
-const truffleAssert = require('truffle-assertions');
+const timeManager = require('openzeppelin-solidity/test/helpers/increaseTime')
+const currentTime = require('openzeppelin-solidity/test/helpers/latestTime')
+const {assertRevert} = require("./helpers")
+const truffleAssert = require('truffle-assertions')
 
 const StateChannel = artifacts.require('StateChannel')
 const computeSignature = (amount, sendingAddress, contractAddress, web3) => {
-    const idString = (amount).toString(16)
-    const hashInput = web3.toHex(contractAddress).slice(2) + '0'.repeat(64 - idString.length) + idString
-    // console.log(idString)
-    const hash = web3.sha3(idString, { encoding: 'hex' })
+  const idString = (amount).toString(16)
+  const hashInput = web3.toHex(contractAddress).slice(2) + '0'.repeat(64 - idString.length) + idString
+  const hash = web3.sha3(hashInput, { encoding: 'hex' })
 
-    // break the signature into its components. For example see: https://ethereum.stackexchange.com/q/15364/4642
-    const signature = web3.eth.sign(sendingAddress, hash);
-    const r = signature.slice(0, 66);
-    const s = '0x' + signature.slice(66, 130);
-    const v =  web3.toDecimal('0x' + signature.slice(130, 132)) + 27
+  // break the signature into its components. For example see: https://ethereum.stackexchange.com/q/15364/4642
+  const signature = web3.eth.sign(sendingAddress, hash)
+  const r = signature.slice(0, 66)
+  const s = '0x' + signature.slice(66, 130)
+  const v =  web3.toDecimal('0x' + signature.slice(130, 132)) + 27
 
-    // this prefix is required by the `ecrecover` builtin solidity function (other than that it is pretty arbitrary)
-    const prefix = "\x19Ethereum Signed Message:\n32";
-    const prefixedBytes = web3.fromAscii(prefix) + hash.slice(2)
-    const prefixedHash = web3.sha3(prefixedBytes, { encoding: 'hex' })
+  // this prefix is required by the `ecrecover` builtin solidity function (other than that it is pretty arbitrary)
+  const prefix = "\x19Ethereum Signed Message:\n32"
+  const prefixedBytes = web3.fromAscii(prefix) + hash.slice(2)
+  const prefixedHash = web3.sha3(prefixedBytes, { encoding: 'hex' })
 
-    return {
-        hash,
-        prefixedHash,
-        signature,
-        r,
-        s,
-        v,
-    }
+  return {
+    hash,
+    prefixedHash,
+    signature,
+    r,
+    s,
+    v,
+  }
 }
 
 contract("StateChannel", (accounts) => {
   let SC
   const sender = accounts[0]
-  const to = accounts[1]
+  const receiver = accounts[1]
   const attacker = accounts[2]
+  const channelValue = 10000000
   let contractAddress
   let value
   describe('getSigner', () => {
     let refSig
     before(async () => {
-      SC = await StateChannel.new(to, 999, { from: sender, value: 10000000 })
+      SC = await StateChannel.new(receiver, 999, { from: sender, value: channelValue })
       contractAddress = SC.address
-      value = Math.floor(Math.random() * 1000)
+      value = Math.floor(Math.random() * channelValue)
 
       refSig = computeSignature(value, sender, contractAddress, web3)
     })
@@ -99,28 +99,20 @@ contract("StateChannel", (accounts) => {
       } = computeSignature(value, sender, attacker, web3)
       const result = await SC.getSigner(prefixedHash, v, r, s)
 
-      // assert(prefixedHash !== refSig.prefixedHash)
-      // assert(r !== refSig.r)
-      // assert(s !== refSig.s)
-      // assert(sender === result)
+      assert(prefixedHash !== refSig.prefixedHash)
+      assert(r !== refSig.r)
+      assert(s !== refSig.s)
+      assert(sender === result)
     })
   })
 
-    // describe('ChannelTimeout', () => {
-    //     let SC
-    //     beforeEach(async () => {
-    //         SC = await StateChannel.new(to, 999, {from: sender, value: 10000000})
-    //     })
-    //     it('should terminate if the Timout has already passed after startDate', async () => {
-    //         const txHash = await  SC.ChannelTimeout.sendTransaction({from:sender});
-    //         const result  = await truffleAssert.createTransactionResult(SC,txHash);
-    //
-    //         await truffleAssert.eventEmitted(result,'TimeOut', (ev) => {
-    //             senderAddress = ev.sender;
-    //             return senderAddress;
-    //         });
-    //
-    //         senderAddress.should.be.equal(sender);
-    //     })
-    // })
+  describe('ChannelTimeout', () => {
+    let SC
+    beforeEach(async () => {
+      SC = await StateChannel.new(receiver, 999, {from: sender, value: 10000000})
+    })
+    it('should revert if the Timout is still active', async () => {
+      await assertRevert(SC.ChannelTimeout.sendTransaction({from:sender}))
+    })
+  })
 })
